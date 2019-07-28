61e14dd5622118aa9b2bc25603a639f5cda9ee71
Matiboux <matiboux@gmail.com>
2019-07-28 20:10:36 +0200

    Handle display behavior on small screens

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,4 +3,12 @@ $(document).ready(function() {
 	var pathname = window.location.pathname;
 	var $link = $('#navbar a[href="' + pathname + '"]');
 	$link.addClass('btn-primary').removeClass('btn-secondary');
+	
+	// Handle display behavior on small screens
+	$(window).resize(function() {
+		if($('#main').width() < 500)
+			$('#main').addClass('smallscreen');
+		else
+			$('#main').removeClass('smallscreen');
+	});
 });
