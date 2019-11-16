1211f2bcb8dc59c47f0b1df4319cedf020dcae8b
Matiboux <matiboux@gmail.com>
2019-11-16 23:18:25 +0100

    Updated the newsline to act as a carousel + Added an ad for Past-Exams

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,6 +6,18 @@ $(document).ready(function() {
 	if($link.hasClass('dropdown-item'))
 		$link.parents('.nav-item').find('.nav-link').addClass('active');
 	
+	// Activate newsline carousel
+	// $('#newsline').carousel(); // Carousel from Bootstrap is glitched
+	var carouselInterval = 10000; // ms
+	setInterval(function() {
+		$current = $('#newsline .carousel-item.active');
+		$current.removeClass('active');
+		$next = $current.nextAll('.carousel-item').first();
+		if($next.length <= 0) $next = $('.carousel-item').first();
+		$next.addClass('active');
+	}, carouselInterval);
+	
+	
 	// Handle display behavior on small screens
 	var checkScreenSize = function() {
 		var $main = $('#main');
@@ -49,7 +61,9 @@ $(document).ready(function() {
 	if (window.matchMedia('(display-mode: standalone)').matches) {
 		$('a:external').attr('target', '_blank');
 	}
+	
+	// Display ad about the web app in the newsline carousel
 	else {
-		$('#addToHomeScreen').show();
+		$('#addToHomeScreen').addClass('carousel-item').removeClass('d-none');
 	}
 });
