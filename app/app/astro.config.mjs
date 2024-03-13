9c72b1faa9eb3743d2d87837a8b945333dc2eb59
Matiboux <matiboux@gmail.com>
2024-03-13 01:30:25 +0100

    Add tailwind integration

--- app/app/astro.config.mjs
+++ app/app/astro.config.mjs
@@ -1,6 +1,9 @@
 import { defineConfig } from 'astro/config'
+import tailwind from '@astrojs/tailwind'
 
 // https://astro.build/config
 export default defineConfig({
-	integrations: [],
+	integrations: [
+		tailwind(),
+	],
 })
