c140ebce205eaf5972786077ed810158d2350563
Matiboux <matiboux@gmail.com>
2024-03-15 00:57:37 +0100

    Run carousel step once before interval

--- app/src/scripts/script.js
+++ app/src/scripts/script.js
@@ -5,27 +5,30 @@ $(document).ready(function() {
 	$link.addClass('active');
 	if($link.hasClass('dropdown-item'))
 		$link.parents('.nav-item').find('.nav-link').addClass('active');
-	
+
 	// Activate newsline carousel
 	// $('#newsline').carousel(); // Carousel from Bootstrap is glitched
-	var carouselInterval = 10000; // ms
-	setInterval(function() {
+	var carouselDelay = 10000; // ms
+	var carouselTimemout;
+	function carouselStep() {
 		$current = $('#newsline .carousel-item.active');
 		$current.removeClass('active');
 		$next = $current.nextAll('.carousel-item').first();
 		if($next.length <= 0) $next = $('.carousel-item').first();
 		$next.addClass('active');
-	}, carouselInterval);
-	
+		carouselTimemout = setTimeout(carouselStep, carouselDelay);
+	}
+	carouselStep();
+
 	// Handle display behavior on small screens
 	var checkScreenSize = function() {
 		var $main = $('#main');
 		var $tiles = $('#main .tiles-grid');
-		
+
 		// $main.width() = $tiles.width() + 30px
 		if($main.width() < 500) {
 			$main.addClass('smallscreen');
-			
+
 			if($main.width() < 340)
 				$tiles.css('max-width', '150px');
 			else
@@ -38,26 +41,26 @@ $(document).ready(function() {
 	};
 	checkScreenSize(); // Execute once after script load
 	$(window).resize(checkScreenSize);
-	
+
 	// jQuery addon for selecting external links
 	$.expr[':'].external = function(a) {
 		var regex = /^(\w+:)?\/\//;
 		var href = $(a).attr('href');
 		return href !== undefined && href.search(regex) !== -1;
 	};
-	
+
 	// Handle Tiles Grid link events for Analytics
 	$('.tiles-grid').on('click', 'a:external', function() {
 		if ($(this).hasClass('invalid'))
 			return; // Ignore clicks on invalid links
-		
+
 		gtag('event', 'click', {
 			event_category: 'Outbound Link',
 			event_label: $(this).find('.branding-bar').text(),
 			transport_type: 'beacon'
 		});
 	});
-	
+
 	// Handle other External link events for Analytics
 	$('body').on('click', 'a:external', function() {
 		// Check that the element in not in the tiles grid
@@ -69,7 +72,7 @@ $(document).ready(function() {
 			});
 		}
 	});
-	
+
 	if (window.matchMedia('(display-mode: standalone)').matches) {
 		// From the web app
 		// Open all external links in a new tab
@@ -79,15 +82,15 @@ $(document).ready(function() {
 		// Display ad about the web app in the newsline carousel
 		$('#addToHomeScreen').addClass('carousel-item').removeClass('d-none');
 	}
-	
+
 	// Handle clicks on Tiles Grid invalid links to warn the user
 	$('.tiles-grid').on('click', 'a.invalid', function(e) {
 		e.preventDefault();
-		
+
 		const brand = $(this).find('.branding-bar').text();
 		const href = $(this).attr('href');
 		const target = $(this).attr('target');
-		
+
 		$('#warn-invalid-brand').text(brand);
 		$('#warn-invalid-href').text(href);
 		$('#warn-invalid-link').attr({ 'href': href, 'target': target });
