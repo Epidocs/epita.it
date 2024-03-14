6d501915c9a32fc91c95ad8b3d04d1899d0d1a38
Matiboux <matiboux@gmail.com>
2024-03-14 23:10:01 +0100

    Add icons used in YAML files in Tailwind safelist

--- app/tailwind.config.mjs
+++ app/tailwind.config.mjs
@@ -14,4 +14,12 @@ export default {
 	plugins: [
 		addDynamicIconSelectors(),
 	],
+	safelist: [
+		'icon-[fa6-solid--bullhorn]',
+		'icon-[fa6-solid--list-ul]',
+		'icon-[fa6-solid--star]',
+		'icon-[fa6-solid--user-group]',
+		'icon-[fa-brands--discord]',
+		'icon-[fa-brands--facebook]',
+	],
 }
