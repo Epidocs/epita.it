0aece8f45c5fa775b46d5af3f44a66dff2333a44
Matiboux <matiboux@gmail.com>
2025-08-20 12:46:11 +0200

    Update astro config

--- app/app/astro.config.mjs
+++ app/app/astro.config.mjs
@@ -1,18 +1,30 @@
 import { defineConfig, envField } from 'astro/config'
 import tailwindcss from '@tailwindcss/vite'
 
+import { i18n } from '/src/config'
+
 // https://astro.build/config
 export default defineConfig({
+	site: process.env.ASTRO_SITE_URL || undefined,
+	base: process.env.ASTRO_BASE_PATH || undefined,
+	build: {
+		assetsPrefix: process.env.ASTRO_ASSETS_PREFIX || undefined,
+	},
 	integrations: [],
 	vite: {
 		plugins: [
 			tailwindcss(),
 		],
 	},
+	i18n: i18n,
 	env: {
 		schema: {
+			// Deployment configuration
 			GITHUB_REPOSITORY_URL: envField.string({ context: 'client', access: 'public', optional: true }),
 			GITHUB_SHA: envField.string({ context: 'client', access: 'public', optional: true }),
+			VERSION_TAG: envField.string({ context: 'client', access: 'public', optional: true }),
+			// Application configuration
+			// Add env vars for your application here.
 		},
 		validateSecrets: true,
 	},
