8343b674be6ab0c3d6a2e966633f6be6c8e23c17
Matiboux <matiboux@gmail.com>
2025-08-20 12:30:30 +0200

    Fix cli entrypoint script

--- app/docker/cli-entrypoint.sh
+++ app/docker/cli-entrypoint.sh
@@ -3,20 +3,21 @@ set -e
 
 if (
 	# If the first argument is a flag,
-	# Assume that the user wants to run npm
 	[ "${1#-}" != "$1" ] ||
+	# Or,
 	(
 		# Unless the first argument is one of these commands,
 		[ "$1" != 'npm' ] &&
 		[ "$1" != 'npx' ] &&
+		[ "$1" != 'pnpm' ] &&
 		[ "$1" != 'node' ] &&
 		# And, unless the first argument is one of these shells,
 		[ "$1" != 'sh' ] &&
 		[ "$1" != 'bash' ]
-		# Assume that the user wants to run npm
 	)
+	# Assume that the user wants to run pnpm
 ); then
-	set -- npm "$@"
+	set -- pnpm "$@"
 fi
 
 exec docker-entrypoint.sh "$@"
