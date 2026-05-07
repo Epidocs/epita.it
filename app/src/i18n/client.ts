bc6b4f3466b61656a2379a39a86c96ff55ddbaae
Matiboux <matiboux@gmail.com>
2026-05-07 12:45:55 +0200

    Add i18n client exports

---
+++ app/src/i18n/client.ts
@@ -0,0 +1,15 @@
+import { defaultLocale, locales, i18n } from './i18n'
+import { i18nFactory } from './i18nFactory'
+import type { Locales, I18nKeys } from './types.d.ts'
+
+export {
+	i18n,
+	defaultLocale,
+	locales,
+	i18nFactory,
+}
+
+export type {
+	Locales,
+	I18nKeys
+}
