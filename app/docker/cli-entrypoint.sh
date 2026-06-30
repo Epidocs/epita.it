8e2d71b9b699b04ecad7d458190a96caa12f7d14
Matiboux <matiboux@gmail.com>
2026-07-01 00:18:21 +0200

    Update cli entrypoint script

--- app/docker/cli-entrypoint.sh
+++ app/docker/cli-entrypoint.sh
@@ -1,21 +1,31 @@
 #!/bin/sh
 set -e
 
+# If the first argument is empty,
+# Run bash
 if (
-	# If the first argument is a flag,
-	[ "${1#-}" != "$1" ] ||
-	# Or,
-	(
-		# Unless the first argument is one of these commands,
-		[ "$1" != 'npm' ] &&
-		[ "$1" != 'npx' ] &&
-		[ "$1" != 'pnpm' ] &&
-		[ "$1" != 'node' ] &&
-		# And, unless the first argument is one of these shells,
-		[ "$1" != 'sh' ] &&
-		[ "$1" != 'bash' ]
-	)
-	# Assume that the user wants to run pnpm
+	[ -z "$1" ]
+); then
+	set -- bash
+
+# If the first argument is a known command or shell,
+# Run with the provided arguments as is
+elif (
+	# Known commands
+	[ "$1" = 'npm' ] ||
+	[ "$1" = 'npx' ] ||
+	[ "$1" = 'pnpm' ] ||
+	[ "$1" = 'node' ] ||
+	# Known shells
+	[ "$1" = 'sh' ] ||
+	[ "$1" = 'bash' ]
+); then
+	:
+
+# If the first argument is a flag,
+# Run pnpm with the provided arguments
+elif (
+	[ "${1#-}" != "$1" ]
 ); then
 	set -- pnpm "$@"
 fi
